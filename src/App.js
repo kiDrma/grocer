545c45675f847d840a32009f98962059fcb9decc
Kim <kimdonnarumma@gmail.com>
2024-07-26 13:21:05 -0400

    five letters forward + backspace working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useRef} from 'react'
 
+
+
+export default function App() {
+
+  const curLetter = useRef([]);
+  const inputs = [0, 1, 2, 3, 4];
+
+  const handler = (e, index) => {
+    if (e.target.value.length >= 1 && index < curLetter.current.length - 1){
+      curLetter.current[index + 1].focus();
+    }
+  }
+
+  const handlerBackspace = (e, index) => {
+    if(e.key === 'Backspace' && index > 0 && e.target.value.length === 0){
+      curLetter.current[index - 1].focus();
+    }
+  }
+
+  return (
+  <div className="App"><h1>Title</h1> 
+    {inputs.map((_, index) => (
+        <input
+          key={index}
+          className="letterInput"
+          ref={el => curLetter.current[index] = el}
+          maxLength={1}
+          onChange={(e) => handler(e, index)}
+          onKeyDown={(e) => handlerBackspace(e, index)}
+        />
+      ))}
+  </div>
+);
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -23,3 +60,4 @@ function App() {
 }
 
 export default App;
+*/
