ae1253fb0530b23678ee6f5f1ddb00a48e95823e
Kim <kimdonnarumma@gmail.com>
2024-07-29 14:40:22 -0400

    changed concept. add to list functional

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useRef} from 'react'
+import {useState} from 'react'
 
 
 
 export default function App() {
-  const curLetter = useRef([]);
-  const inputs = [0, 1, 2, 3, 4];
+  const [list, updateList] = useState([]);
+  const [newFood, setNewFood] = useState('');
 
-  const handler = (e, index) => {
-    if (e.target.value.length >= 1 && index < curLetter.current.length - 1){
-      curLetter.current[index + 1].focus();
+  function buttonHandler(){
+    if(newFood != ''){
+      updateList([...list, newFood]);
     }
-  }
-
-  const handlerBackspace = (e, index) => {
-    if(e.key === 'Backspace' && index > 0 && e.target.value.length === 0){
-      curLetter.current[index - 1].focus();
-    }
+    setNewFood('');
   }
 
   return (
     <div className="App">
       <h1 className="title">Title</h1> 
-      {inputs.map((_, index) => (
-          <input
-            key={index}
-            className="letterInput"
-            ref={el => curLetter.current[index] = el}
-            maxLength={1}
-            onChange={(e) => handler(e, index)}
-            onKeyDown={(e) => handlerBackspace(e, index)}
-          />
-        ))}
-      <br/>
-      <button className="inputButton"> hello</button>
+
+      <input 
+      type="text" value={newFood}
+      onChange={(change) => setNewFood(change.target.value)}
+      className="foodInput">
+      </input> 
+
+      <button onClick={buttonHandler}>Enter</button>
+
+      <div>
+        <ul className="groceryList">
+        {list.map((item, index) => (
+            <li key={index}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
